b013046175566963391801d313dd3b9a1ca5fec4
f-zafar <fatimazafar2005@gmail.com>
2025-03-13 05:15:28 +0100

    Initial commit

---
+++ auction_project.py
@@ -0,0 +1,30 @@
+# Secret Auction Project
+
+from auction_art import hammer
+print(hammer)
+print("Welcome to the secret auction!")
+def find_highest_bidder(bidding_dictionary):
+    winner = ""
+    highest_bid = 0
+
+    for bidder in bidding_dictionary:
+        bid_amount = bidding_dictionary[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder 
+    
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+bids = {}
+
+continue_bidding = True
+while continue_bidding:
+    name = input("What is your name?: ")
+    price = float(input("What is your bid?: $ "))
+    bids[name] = price
+    people  = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+    if people == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif people == "yes":
+        print("\n" * 20)
